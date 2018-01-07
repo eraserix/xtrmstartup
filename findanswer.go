01555326481cb7d7e6763271948dcf001d07c28a
Christoph Mathys <cmathys@gmail.com>
2018-01-07 20:42:41 +0100

    add implementation to find largest number

--- findanswer.go
+++ findanswer.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func findTheAnswer(question string) string {
-	return "go"
+	if strings.HasPrefix(question, "which of the following numbers is the largest") {
+		return findLargest(question)
+	} else if strings.HasPrefix(question, "what is your name") {
+		return "go"
+	}
+	return ""
+}
+
+func findLargest(question string) string {
+	numbers := strings.Split(strings.Split(question, ":")[1], ",")
+
+	maxV := 0
+	for _, n := range numbers {
+		parsed, _ := strconv.Atoi(strings.TrimSpace(n))
+		maxV = max(parsed, maxV)
+	}
+	return strconv.Itoa(maxV)
 }
--- findanswer_test.go
+++ findanswer_test.go
@@ -8,3 +8,10 @@ func TestFindName(t *testing.T) {
 		t.Errorf("name not equel, expected go, found %v", name)
 	}
 }
+
+func TestFindLargest(t *testing.T) {
+	largest := findTheAnswer("which of the following numbers is the largest: 464, 54, 3, 8")
+	if largest != "464" {
+		t.Errorf("largest number was %v, expected 464", largest)
+	}
+}
