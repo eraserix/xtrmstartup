e9cca77d2e4b645e2802d785c30e4ec9d298f365
Christoph Mathys <cmathys@gmail.com>
2018-01-20 16:03:22 +0100

    split calculations into separate file

--- findanswer.go
+++ findanswer.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/big"
 	"reflect"
 	"regexp"
 	"sort"
@@ -93,52 +92,6 @@ func findPrimes(question string) string {
 		}
 	}
 	return strings.Join(strPrimes, ", ")
-}
-
-func doPlusQuestion(question string) string {
-	var a, b, c int
-	switch {
-	case strings.Count(question, "plus") == 2:
-		fmt.Sscanf(question, "what is %d plus %d plus %d", &a, &b, &c)
-		return strconv.Itoa(a + b + c)
-	case strings.Count(question, "plus") == 1 &&
-		strings.Count(question, "multiplied by") == 1:
-		_, e := fmt.Sscanf(question, "what is %d plus %d multiplied by %d", &a, &b, &c)
-		if e == nil {
-			return strconv.Itoa(a + b*c)
-		}
-		_, e = fmt.Sscanf(question, "what is %d multiplied by %d plus %d", &a, &b, &c)
-		if e == nil {
-			return strconv.Itoa(a*b + c)
-		}
-	case strings.Count(question, "plus") == 1:
-		fmt.Sscanf(question, "what is %d plus %d", &a, &b)
-		return strconv.Itoa(a + b)
-	}
-	return ""
-}
-
-func doSimpleCalculation(question string) string {
-	var a, b int
-	switch {
-	case strings.Contains(question, "plus"):
-		return doPlusQuestion(question)
-	case strings.Contains(question, "minus"):
-		fmt.Sscanf(question, "what is %d minus %d", &a, &b)
-		return strconv.Itoa(a - b)
-	case strings.Contains(question, "multiplied by"):
-		fmt.Sscanf(question, "what is %d multiplied by %d", &a, &b)
-		return strconv.Itoa(a * b)
-	case strings.Contains(question, "to the power of"):
-		fmt.Sscanf(question, "what is %d to the power of %d", &a, &b)
-		base := new(big.Int).SetInt64(int64(a))
-		result := new(big.Int).Set(base)
-		for i := 1; i < b; i++ {
-			result.Mul(result, base)
-		}
-		return result.String()
-	}
-	return ""
 }
 
 func findFibonacci(question string) string {
